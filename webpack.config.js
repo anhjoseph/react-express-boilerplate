71c130ad0b3aaf3d0429f5d6cb3a51727dd4ff8c
Joseph An <josephan1221@gmail.com>
2018-09-16 20:46:43 -0700

    Update webpack to cleaner config

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,39 @@ const BUILD_DIR = path.resolve(__dirname, './client/dist');
 module.exports = {
   entry: path.resolve(SRC_DIR, 'index.jsx'),
   output: {
-    filename: 'bundle.js',
     path: BUILD_DIR,
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'env'],
+            },
           },
-        },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+        ],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
   },
 };
